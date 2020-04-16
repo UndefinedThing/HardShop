506522187af55379fd4ab1d893ecf162fd501f04
TheOnlyOne <martin.petit123@outlook.fr>
2020-04-16 10:25:33 +0200

    Auto stash before merge of "master" and "origin/master"

--- src/App.js
+++ src/App.js
@@ -3,42 +3,26 @@ import "./App.css";
 import "firebase/firestore";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-
 import Home from "./pages/home";
 import CRUD from "./components/CRUD/CRUD";
-
-const firebase = require("firebase");
-require("firebase/firestore");
-
-var FirebaseConfig = {
-  apiKey: "AIzaSyAJDdWdPSXRxTukWUhYqrLh44HqBCXl4F4",
-  authDomain: "hardware-web.firebaseapp.com",
-  databaseURL: "https://hardware-web.firebaseio.com",
-  projectId: "hardware-web",
-  storageBucket: "hardware-web.appspot.com",
-  messagingSenderId: "762528731852",
-  appId: "1:762528731852:web:46def58e7b0c6606136869",
-  measurementId: "G-19KQ1NJ1N2",
-};
-if (firebase.apps.length === 0) {
-  firebase.initializeApp(FirebaseConfig);
-}
+import Connection from "./pages/ConnectPage";
 
 function App() {
   return (
-    <div>      
-        <Router>
-            <Switch>
-              <Route exact path="/">
-                <Home />
-              </Route>
-              <Route path="/CRUDs">
-                <CRUD />
-              </Route>
-              <Route path="/Cards">
-              </Route>
-            </Switch>
-        </Router>
+    <div>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/CRUDs">
+            <CRUD />
+          </Route>
+          <Route path="/Auth">
+            <Connection />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
--- src/components/header.js
+++ src/components/header.js
@@ -6,47 +6,47 @@ class Header extends Component {
   render() {
     return (
       <header className="header header-sticky header-transparent">
-              
-              
-              <Navbar collapseOnSelect bg="dark" expand="lg" variant="dark">
-                <a href="/" className="navbar-brand">
-                  <img src={hardware} alt="Logo" />
-                </a>
-                <Navbar.Brand href="/">HardShop</Navbar.Brand>
-                <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
-                <Navbar.Collapse id="responsive-navbar-nav">
-                <Nav className="mr-auto">
-                  <Nav.Link href="/">Home</Nav.Link>
-                  <Nav.Link href="">News</Nav.Link>
-                  <Nav.Link href="">Configuration</Nav.Link>
-                  <NavDropdown title="Composants" id="collasible-nav-dropdown">
-                    <NavDropdown.Item href="">Processeur</NavDropdown.Item>
-                    <NavDropdown.Item href="">Carte Mère</NavDropdown.Item>
-                    <NavDropdown.Item href="">RAM</NavDropdown.Item>
-                    <NavDropdown.Item href="">Carte Graphique</NavDropdown.Item>
-                    <NavDropdown.Item href="">Refroidissement</NavDropdown.Item>
-                    <NavDropdown.Item href="">SSD</NavDropdown.Item>
-                    <NavDropdown.Item href="">Alimentation PC</NavDropdown.Item>
-                    <NavDropdown.Item href="">Boîtier</NavDropdown.Item>
-                    <NavDropdown.Item href="">HDD</NavDropdown.Item>
-                    <NavDropdown.Item href="">OS</NavDropdown.Item>
-                  </NavDropdown>
-                  <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-                    <NavDropdown.Item href="/CRUDs">Config</NavDropdown.Item>
-                    <NavDropdown.Item href="/Cards">Cards</NavDropdown.Item>
-                    <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                    <NavDropdown.Divider />
-                    <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                  </NavDropdown>
-                </Nav>
-                <Nav>
-                  <Nav.Link href="">Recherche</Nav.Link>
-                  <Nav.Link href="">
-                    Connexion
-                  </Nav.Link>
-                </Nav>
-                </Navbar.Collapse>
-              </Navbar>
+        <Navbar collapseOnSelect bg="dark" expand="lg" variant="dark">
+          <a href="/" className="navbar-brand">
+            <img src={hardware} alt="Logo" />
+          </a>
+          <Navbar.Brand href="/">HardShop</Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="">News</Nav.Link>
+              <Nav.Link href="">Configuration</Nav.Link>
+              <NavDropdown title="Composants" id="collasible-nav-dropdown">
+                <NavDropdown.Item href="">Processeur</NavDropdown.Item>
+                <NavDropdown.Item href="">Carte Mère</NavDropdown.Item>
+                <NavDropdown.Item href="">RAM</NavDropdown.Item>
+                <NavDropdown.Item href="">Carte Graphique</NavDropdown.Item>
+                <NavDropdown.Item href="">Refroidissement</NavDropdown.Item>
+                <NavDropdown.Item href="">SSD</NavDropdown.Item>
+                <NavDropdown.Item href="">Alimentation PC</NavDropdown.Item>
+                <NavDropdown.Item href="">Boîtier</NavDropdown.Item>
+                <NavDropdown.Item href="">HDD</NavDropdown.Item>
+                <NavDropdown.Item href="">OS</NavDropdown.Item>
+              </NavDropdown>
+              <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+                <NavDropdown.Item href="/CRUDs">Config</NavDropdown.Item>
+                <NavDropdown.Item href="/boutique">Boutique</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">
+                  Something
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">
+                  Separated link
+                </NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+            <Nav>
+              <Nav.Link href="">Recherche</Nav.Link>
+              <Nav.Link href="/Auth">Connexion</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
       </header>
     );
   }
