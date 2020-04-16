1cdc0b6837f90f8b22f175f43b756375eba141ff
FrekiManagarm <mathieu.chambaud@ynov.com>
2020-04-16 13:59:38 +0200

    ajout des pages d'affichage de chaque composants

--- src/components/header.js
+++ src/components/header.js
@@ -29,16 +29,9 @@ class Header extends Component {
                 <NavDropdown.Item href="">HDD</NavDropdown.Item>
                 <NavDropdown.Item href="">OS</NavDropdown.Item>
               </NavDropdown>
-              <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+              <NavDropdown title="Paramètres" id="collasible-nav-dropdown">
                 <NavDropdown.Item href="/CRUDs">Config</NavDropdown.Item>
                 <NavDropdown.Item href="/boutique">Boutique</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">
-                  Something
-                </NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">
-                  Separated link
-                </NavDropdown.Item>
               </NavDropdown>
             </Nav>
             <Nav>
---
+++ src/pages/pages_composants/Alimentation.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "../../components/header";
+import Footer from "../../components/footer";
+
+class Alim extends React.Component {
+    render() {
+        return(
+            <div>
+                <Header />
+                
+            </div>
+        )
+    }
+}
+
+export default Alim;
