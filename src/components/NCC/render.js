19dbd97472af78a5ff6af37167c5ca788e1d8920
TheOnlyOne <martin.petit123@outlook.fr>
2020-04-28 16:51:28 +0200

    loul

--- src/components/NCC/render.js
+++ src/components/NCC/render.js
@@ -1,31 +1,9 @@
-import React, { Component } from 'react';
-import { Tabs, Tab, TabPanel, TabList } from 'react-web-tabs';
+import React, { Component } from "react";
 import Cards from "./Composants";
-
 
 class Cat extends Component {
   render() {
-    return (
-      <Tabs
-        defaultTab="Config"
-        onChange={(tabId) => { console.log(tabId) }}
-      >
-        <TabList>
-          <Tab tabFor="Config">Config</Tab>
-          <Tab tabFor="Composants">Composants</Tab>
-          <Tab tabFor="News">News</Tab>
-        </TabList>
-        <TabPanel tabId="Config">
-          <p>Tab 1 content</p>
-        </TabPanel>
-        <TabPanel tabId="Composants">
-          <Cards />
-        </TabPanel>
-        <TabPanel tabId="News">
-          <p>Tab 3 content</p>
-        </TabPanel>
-      </Tabs>
-    );
+    return <p>Bonjour</p>;
   }
 }
 
